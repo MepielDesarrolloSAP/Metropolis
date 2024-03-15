434dc05a7b2fb87bb9d0b1973ef952594e5d3186
A.Jimenez <abraham.jimenez@mepiel.com.mx>
2024-03-15 10:04:46 -0600

    Changes

---
+++ Gate/Clases/VisitsimpleRoutePart.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Gate.Clases
+{
+    public class VisitsimpleRoutePart
+    {
+        public decimal Id_SimpleRoutevisit { get; set; }
+        public string Id_SimpleRouteRoute { get; set; }
+        public decimal Id_DriverSimpleRoute { get; set; }
+        public string Pictures { get; set; }
+        public string ETime_arrival { get; set; }
+        public string ReferenceDocNums { get; set; }
+    }
+}
